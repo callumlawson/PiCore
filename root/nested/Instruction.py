a17057c91dd98be6e2f180138bac00b56e95b1e6
Lawrence Esswood <esswoodl@gmail.com>
2012-09-07 17:32:47 +0100

    Another test that makes no real difference

--- root/nested/Instruction.py
+++ root/nested/Instruction.py
@@ -4,7 +4,7 @@ Created on 7 Sep 2012
 @author: Callum
 '''
 class Instruction:
-    name = ""
+    name = "nop"
     values = []
     valueMods = []
     def __init__(self, instructionName = "nop", instructionValues = [0], instructionMods = [""]):
