603101d6b5382f414e9f9adcd40837c602daf80e
Lawrence Esswood <esswoodl@gmail.com>
2012-09-15 00:03:33 +0100

    Part render + render optimisations

--- root/nested/GameEngine.py
+++ root/nested/GameEngine.py
@@ -22,6 +22,13 @@ class GameEngine(object):
     def __init__(self, pygameDisplay):
         self.programNames = []
         
+        #Parser
+        parser = Parser()
+        testCode = parser.processFile("demoCode.txt")
+        for instruction in testCode:
+            print instruction.printInstruction()
+        self.newCoreGame(10000, [testCode], ["demoCode"])
+        
         self.menuHeight = 70
         
         self.display = pygameDisplay
@@ -34,13 +41,9 @@ class GameEngine(object):
         self.app = MainGui(self.display,(self.screenWidth,self.screenHeight),self.menuHeight)
         self.app.engine = self
         
-        #Parser
-        parser = Parser()
-        testCode = parser.processFile("demoCode.txt")
-        for instruction in testCode:
-            print instruction.printInstruction()
-        self.newCoreGame(1000, [testCode], ["demoCode"])
-
+        self.rect = self.app.get_render_area()
+        
+        self.doResize()
 
     def newCoreGame(self, size, programs, names):
         self.virtualCore = VirtualCore(size)
@@ -59,61 +62,54 @@ class GameEngine(object):
     def resume(self):
         print "resume"
         self.clock.resume()
-
-    def render(self, dest, rect): #Do the drawing stuff!
         
-        # Draw a rotating drawArea
-        #angle = self.clock.get_time()*10
-        #r = surf.get_rect()
-        #r.center = rect.center
-        #dest.fill((0,0,0), rect)
-        #angle = 90
-        #surf = pygame.transform.rotozoom(self.drawArea, angle, 1)
-
-        pygame.draw.rect(self.drawArea,pygame.Color(0,0,0),(0,0,self.screenWidth,self.screenHeight-self.menuHeight))
+    def doResize(self):
         
-        #TODO the self.screenHeight is stil wrong because of the menu. But otherwise this works
-        
-        #I did some maths =p. Lets w is the width of the screen, h the height. X the number in a row, Y the number in a column. N the number of squares
-        #We would like the ratio of w:h to be equal to x:y. So (w/h) = (x/y).
-        #Lets assume that we end up with roughly a square, so xy = N
-        # via substitution we get x = root(wN/h). 
-        
-        numberInX = math.ceil(math.sqrt((self.screenWidth * self.virtualCore.size)/(self.screenHeight-self.menuHeight))) #ratio of x rounded up
-        numberInY = math.ceil(self.virtualCore.size/numberInX) # and so in Y
+        self.numberInX = math.ceil(math.sqrt((self.screenWidth * self.virtualCore.size)/(self.screenHeight-self.menuHeight))) #ratio of x rounded up
+        self.numberInY = math.ceil(self.virtualCore.size/self.numberInX) # and so in Y
         
         #the 'perfect' size for each cell would be
-        xSize = self.screenWidth/numberInX
-        ySize = (self.screenHeight-self.menuHeight)/numberInY
+        xSize = self.screenWidth/self.numberInX
+        ySize = (self.screenHeight-self.menuHeight)/self.numberInY
         
         #choose the smaller one to make squares
         if(xSize < ySize):
             self.squareSize = xSize
         else:
             self.squareSize = ySize
-        
+        self.fullRender()
+    def partRender(self):
+        for index in self.virtualCore.getChanges():
+            y = int(index/self.numberInX)
+            x = int(index%self.numberInX)
+            pygame.draw.rect(self.drawArea,pygame.Color(0,0,0) ,(x*(self.squareSize), y*(self.squareSize),self.squareSize+self.padding,self.squareSize+self.padding), self.padding/2)
+            self.renderRectangle(x, y, index)
+        self.virtualCore.clearChanges()
+        self.display.blit(self.drawArea,(0,0))
+        return (self.rect, )
+    def fullRender(self): #Do the drawing stuff!
+        self.virtualCore.clearChanges()
+        pygame.draw.rect(self.drawArea,pygame.Color(0,0,0),(0,0,self.screenWidth,self.screenHeight-self.menuHeight))
+       
         count = 0
-        for y in xrange(int(numberInY)):
-            for x in xrange(int(numberInX)):
-                instruction = self.virtualCore.memory[count]
-               
-                if instruction.lastMod == -1:
-                    pygame.draw.rect(self.drawArea,pygame.Color(200,200,200) ,(self.padding + x*(self.squareSize),self.padding + y*(self.squareSize),self.squareSize-self.padding,self.squareSize-self.padding), 0)
-                else:
-                    pygame.draw.rect(self.drawArea,self.colors[instruction.lastMod] ,(self.padding + x*(self.squareSize),self.padding + y*(self.squareSize),self.squareSize-self.padding,self.squareSize-self.padding), 0)
-                for playerCounter in self.virtualCore.playerCounters: #This could be done quicker
-                    if count in playerCounter.counters:
-                        pygame.draw.rect(self.drawArea,pygame.Color(255,255,0) ,(x*(self.squareSize), y*(self.squareSize),self.squareSize+self.padding,self.squareSize+self.padding), self.padding/2)
-                        
+        for y in xrange(int(self.numberInY)):
+            for x in xrange(int(self.numberInX)):
+                self.renderRectangle(x, y, count)       
                 count +=1
                 if(count == self.virtualCore.size):
                     break;
         self.display.blit(self.drawArea,(0,0))
-        return (rect,)
-    
-    def gameOver(self):
-        pass
-    
+        return (self.rect, )
+    def renderRectangle(self, x, y, index):
+        instruction = self.virtualCore.memory[index]
+        if instruction.lastMod == -1:
+            pygame.draw.rect(self.drawArea,pygame.Color(200,200,200) ,(self.padding + x*(self.squareSize),self.padding + y*(self.squareSize),self.squareSize-self.padding,self.squareSize-self.padding), 0)
+        else:
+            pygame.draw.rect(self.drawArea,self.colors[instruction.lastMod] ,(self.padding + x*(self.squareSize),self.padding + y*(self.squareSize),self.squareSize-self.padding,self.squareSize-self.padding), 0)
+        for playerCounter in self.virtualCore.playerCounters: #This could be done quicker
+            if index in playerCounter.counters:
+                pygame.draw.rect(self.drawArea,pygame.Color(255,255,0) ,(x*(self.squareSize), y*(self.squareSize),self.squareSize+self.padding,self.squareSize+self.padding), self.padding/2)
+                
     def run(self):
         self.app.update()
         pygame.display.flip()
@@ -122,7 +118,8 @@ class GameEngine(object):
         
         # Main program loop
         done = False
-        while not done:                      
+        while not done:
+            self.rect = self.app.get_render_area()                       
             #Update the game
             if not self.clock.paused and self.virtualCore != None: 
                 ret = self.virtualCore.tick()
@@ -142,26 +139,28 @@ class GameEngine(object):
                     pygame.display.update()
                     print self.screenWidth,self.screenHeight
                     self.app.event(event)
+                    self.doResize()
                 else:
                     # Pass the event off to pgu
                     self.app.event(event)
                     
             # Render the game
-            rect = self.app.get_render_area()
             updates = []
-            self.display.set_clip(rect)
-            lst = self.render(self.display, rect)
+            self.display.set_clip(self.rect)
+            lst = self.partRender()
             if (lst):
                 updates += lst
+                
             self.display.set_clip()
 
             # Cap it at 30fps
             #self.clock.tick(25)
+
             # Give pgu a chance to update the display (menu)
             lst = self.app.update()
             if (lst):
                 updates += lst
             pygame.display.update(updates)
             
-            pygame.time.wait(0)
+            pygame.time.wait(1000)
 
