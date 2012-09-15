75d83c8ba166d3c8277ec370ae75f5a549634bd8
Lawrence Esswood <esswoodl@gmail.com>
2012-09-15 17:47:24 +0100

    Added ROM, fiddled with playerProgramCounter

--- root/nested/PlayerProgramCounter.py
+++ root/nested/PlayerProgramCounter.py
@@ -14,30 +14,35 @@ class PlayerProgramCounter:
         self.counters = [startPointer]
         self.currentCounter = 0
         self.core = owner
+        self.core.ROM[1] +=1
+        self.core.ROM[2] +=1
     def currentPointer(self):
         return self.counters[self.currentCounter]
     
     def killCurrentPointer(self): # returns true if that was the last pointer
+        self.core.ROM[2] -=1
         self.counters.pop(self.currentCounter)
         remain = len(self.counters)
         if(remain > 0):
             self.currentCounter %=remain
             return False
+        self.core.ROM[1] -=1
         return True
     
     def jumpPointer(self, value):
         self.counters[self.currentCounter] = value
         self.core.changesList.append(value)
-    def advancePointer(self, memmorySize):
-        self.counters[self.currentCounter] = (self.counters[self.currentCounter] + 1)%memmorySize
+    def advancePointer(self):
+        self.counters[self.currentCounter] = (self.counters[self.currentCounter] + 1)%self.core.size
         self.core.changesList.append(self.counters[self.currentCounter])
     def advanceThread(self):
         self.currentCounter = (self.currentCounter+1)%len(self.counters)
         
-    def advanceBoth(self, memmorySize):
-        self.advancePointer(memmorySize)
+    def advanceBoth(self):
+        self.advancePointer()
         self.advanceThread()
         
     def spawnThread(self, at):
+        self.core.ROM[2] +=1
         self.core.changesList.append(at)
         self.counters.insert(self.currentCounter+1, at)
--- root/nested/VirtualCore.py
+++ root/nested/VirtualCore.py
@@ -13,13 +13,14 @@ class PlayerGone:
         self.playersLeft = Left
         
 class VirtualCore:
-    
+    # ROM = [memSize,processes,threads,clockCount]
     def __init__(self, memorySize):
         self.playerCounters = []
         self.IDcount = 0
         self.memory = [Instruction() for j in range(memorySize)]
         self.size = memorySize
         self.currentPlayer = 0
+        self.ROM = [memorySize,0,0,0]
         self.changesList = []
     def load(self, position, code):
         codeLength = len(code)
@@ -50,11 +51,10 @@ class VirtualCore:
         return None
         
     def execute(self, instruction, instructionLocation):   #will return true if the player has lost his last thread
-        #TODO: execute instructions. Should edit self.memory and call advancePointer and/or advanceThread if relevant
         instruction.lastMod = self.playerCounters[self.currentPlayer].ID
         self.changesList.append(instructionLocation)
         if(instruction.name == "nop"): #do nothing. Duh
-            self.playerCounters[self.currentPlayer].advanceBoth(self.size)
+            self.playerCounters[self.currentPlayer].advanceBoth()
             
         elif(instruction.name == "dat"): #Just. Die.
             return self.playerCounters[self.currentPlayer].killCurrentPointer()
@@ -65,7 +65,7 @@ class VirtualCore:
             if(location == -1): #invalid destination, kills thread
                 return self.playerCounters[self.currentPlayer].killCurrentPointer()
             self.memory[location] = thingToMove.clone()
-            self.playerCounters[self.currentPlayer].advanceBoth(self.size)
+            self.playerCounters[self.currentPlayer].advanceBoth()
             
         elif(instruction.name == "jmp"): #jump program counter to a point in memory
             destination = self.getLocation(instruction.arguments[0], instructionLocation)
@@ -85,7 +85,22 @@ class VirtualCore:
             
         elif(instruction.name == "dvd"): # add
             return self.mathOperation(instruction, instructionLocation, self.divOperation) 
-            
+        
+        elif(instruction.name == "mod"): # add
+            return self.mathOperation(instruction, instructionLocation, self.modOperation) 
+        
+        elif(instruction.name == "and"): # add
+            return self.mathOperation(instruction, instructionLocation, self.andOperation) 
+        
+        elif(instruction.name == "or"): # add
+            return self.mathOperation(instruction, instructionLocation, self.orOperation) 
+        
+        elif(instruction.name == "xor"): # add
+            return self.mathOperation(instruction, instructionLocation, self.xorOperation) 
+        
+        elif(instruction.name == "nnd"): # add
+            return self.mathOperation(instruction, instructionLocation, self.nandOperation) 
+      
         elif(instruction.name == "jpi"): #jump if
             firstargument = self.getInstruction(instruction.arguments[1],instructionLocation)
             secondargument = self.getInstruction(instruction.arguments[2],instructionLocation)
@@ -97,13 +112,13 @@ class VirtualCore:
                 self.playerCounters[self.currentPlayer].jumpPointer(destination)
                 self.playerCounters[self.currentPlayer].advanceThread()
             else:
-                self.playerCounters[self.currentPlayer].advanceBoth(self.size)
+                self.playerCounters[self.currentPlayer].advanceBoth()
                 
         elif(instruction.name == "bch"): # new thread?
             destination = self.getLocation(instruction.arguments[0], instructionLocation)
             if(destination == -1):
                     return self.playerCounters[self.currentPlayer].killCurrentPointer()
-            self.playerCounters[self.currentPlayer].advancePointer(self.size)
+            self.playerCounters[self.currentPlayer].advancePointer()
             self.playerCounters[self.currentPlayer].spawnThread(destination)
             self.playerCounters[self.currentPlayer].advanceThread()
             
@@ -116,7 +131,7 @@ class VirtualCore:
         if(location == -1): #invalid destination, kills thread
             return self.playerCounters[self.currentPlayer].killCurrentPointer()
         self.memory[location].arguments[0] = (self.memory[location].arguments[0][0],op(int(firstargument.arguments[0][1]) , int(secondargument.arguments[0][1]))%self.size)
-        self.playerCounters[self.currentPlayer].advanceBoth(self.size)
+        self.playerCounters[self.currentPlayer].advanceBoth()
         return False
     
     def addOperation(self, op1, op2):
@@ -127,16 +142,27 @@ class VirtualCore:
         return op1 * op2
     def divOperation(self, op1, op2):
         return math.floor(op1 / op2)
-    
+    def modOperation(self, op1, op2):
+        return op1%op2
+    def andOperation(self, op1, op2):
+        return self.boolToInt((op1 != 0) and (op2 != 0))
+    def orOperation(self, op1, op2):
+        return self.boolToInt((op1 != 0) or (op2 != 0))
+    def xorOperation(self, op1, op2):
+        return self.boolToInt((op1 != 0) != (op2 != 0))
+    def nandOperation(self, op1, op2):
+        return self.boolToInt((op1 == 0) and (op2 == 0))
     def getInstruction(self, argumentTuple, relativePoint): #returns the instruction in the relevent location (or makes the pseudo data for literals)
+        if(argumentTuple[0] == "$"):
+            return Instruction("dat", argumentTuple[1]%len(self.ROM))
         atLocation = self.getLocation(argumentTuple, relativePoint)
         if(atLocation == -1):
             return Instruction("dat", [argumentTuple])
-        elif(argumentTuple[0] == "$"):
-            #TODO: stuff concerning read only stuff
-            pass
         return self.memory[atLocation]
-            
+    def boolToInt(self, aBool):
+        if(aBool):
+            return 1
+        return 0        
     def getLocation(self, argumentTuple, relativePoint): #return -1 if not valid
         loc = -1
         if(argumentTuple[0] == ""):
@@ -146,14 +172,12 @@ class VirtualCore:
         elif(argumentTuple[0] == "#"):
             loc = (int(self.memory[(int(argumentTuple[1]) + relativePoint)%self.size].arguments[0][1]) + relativePoint)%self.size
         elif(argumentTuple[0] == "$"):
-            #TODO: stuff concerning read only stuff
-            pass
+            return -1
         self.memory[loc].lastMod = self.playerCounters[self.currentPlayer].ID
         self.changesList.append(loc)
         return loc
     def setROM(self):
-        #TODO: set the read only memory
-        pass
+        self.ROM[3] +=1
 
 
 
